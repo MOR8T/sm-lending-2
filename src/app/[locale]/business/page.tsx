c804fd939095a69ec819adf7987fb68f48cc0e7c
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-08-13 19:55:27 +0500

    create: new component ServicesSection for Business page

--- src/app/[locale]/business/page.tsx
+++ src/app/[locale]/business/page.tsx
@@ -1,10 +1,15 @@
-import { businessCards1 } from "@/constants/business/const";
+"use client"
+import { businessCards1, businessCards2 } from "@/constants/business/const";
+import ServicesSection from "@/ui/business/ServicesSection";
 import СardsRow from "@/ui/business/СardsRow";
+import { useTranslations } from "use-intl";
 
 export default function Business() {
+  const t = useTranslations("ServicesSection")
   return (
     <div>
       <СardsRow cards={businessCards1} />
+      <ServicesSection cards={businessCards2} title={t("title")} />
     </div>
   );
 }
--- src/constants/business/const.ts
+++ src/constants/business/const.ts
@@ -1,21 +1,75 @@
 import businessCardsImg1 from "@/../public/icons/businessCardsImg1.svg";
 import businessCardsImg2 from "@/../public/icons/businessCardsImg2.svg";
 import businessCardsImg3 from "@/../public/icons/businessCardsImg3.svg";
+import ServicesSectionImg1 from "@/../public/icons/ServicesSectionImg1.svg";
+import ServicesSectionImg2 from "@/../public/icons/ServicesSectionImg2.svg";
+import ServicesSectionImg3 from "@/../public/icons/ServicesSectionImg3.svg";
+import ServicesSectionImg4 from "@/../public/icons/ServicesSectionImg4.svg";
+import { stylesServicesSectionT } from "@/types/business/cards";
 
 export const businessCards1 = [
   {
+    title: "title1",
+    subTitle: "subTitle1",
+    img: businessCardsImg1,
+  },
+  {
+    title: "title2",
+    subTitle: "subTitle2",
+    img: businessCardsImg2,
+  },
+  {
+    title: "title3",
+    subTitle: "subTitle3",
+    img: businessCardsImg3,
+  },
+];
+
+export const stylesServicesSection: stylesServicesSectionT = {
+  0: {
+    div: "col-start-1 col-end-2 row-start-1 row-end-2 bg-[#F7F8F9]",
+    h1: "",
+    h2: "",
+    img: "",
+  },
+  1: {
+    div: "col-start-2 col-end-4 row-start-1 row-end-2 bg-[#F4F8FB]",
+    h1: "w-[220px]",
+    h2: "w-[291px]",
+    img: "right-0 !w-[393px]",
+  },
+  2: {
+    div: "col-start-3 col-end-4 row-start-2 row-end-3 bg-[#F7F8F9]",
+    h1: "",
+    h2: "",
+    img: "",
+  },
+  3: {
+    div: "col-start-1 col-end-3 row-start-2 row-end-3 bg-[#DAE6F8]",
+    h1: "w-[209px]",
+    h2: "w-[278px]",
+    img: "right-0 !w-[393px]",
+  },
+};
+export const businessCards2 = [
+  {
     title: `Облегчим работу \n бухгалтера`,
     subTitle: `Ваш бухгалтер начислит зарплату без \nлишних бумаг и усилий`,
-    img: businessCardsImg1,
+    img: ServicesSectionImg1,
   },
   {
     title: `Работайте с любого \n устройства`,
     subTitle: `Пользуйтесь интернет-банкингом в \nлюбом браузере даже через телефон`,
-    img: businessCardsImg2,
+    img: ServicesSectionImg2,
   },
   {
     title: `Следите с любой точки \n Земли`,
     subTitle: `Управляйте прогрессом вашего \nбизнеса, где бы вы ни находились`,
-    img: businessCardsImg3,
+    img: ServicesSectionImg3,
+  },
+  {
+    title: `Следите с любой точки \n Земли`,
+    subTitle: `Управляйте прогрессом вашего \nбизнеса, где бы вы ни находились`,
+    img: ServicesSectionImg4,
   },
 ];
