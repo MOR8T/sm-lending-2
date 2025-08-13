f1c06ef91d3c66d9b3a4fc281e7478eb528a5301
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2025-08-13 12:43:23 +0500

    Make 50% of header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+"use client";
+import { useLocale, useTranslations } from "next-intl";
+import Image from "next/image";
+import React, { useLayoutEffect, useState } from "react";
+import LocaleSwicherSelect from "../localeSwicherSelect/LocaleSwicherSelect";
+import { Link, usePathname } from "@/i18n/navigation";
 
 function Header() {
+  const t = useTranslations("Header");
+  const [activeModule, setActiveModule] = useState("1");
+  const locale = useLocale();
+  const pathname = usePathname();
+
+  const activeModuleClass = "border-b-[2px] border-[#3a6178]";
+
+  useLayoutEffect(() => {
+    if (pathname.includes("/business")) {
+      setActiveModule("2");
+    } else if (pathname.includes("/bank")) {
+      setActiveModule("3");
+    } else {
+      setActiveModule("1");
+    }
+  }, [pathname]);
+
   return (
-    <div>Header</div>
-  )
+    <div>
+      <div className="container flex justify-between md:border-b md:border-b-gray-300 items-center">
+        <div className="flex gap-8 items-center">
+          <Image
+            src={`/images/Logo_${locale}.svg`}
+            alt=""
+            width="271"
+            height="45"
+          />
+          <ul className="flex gap-5">
+            <li
+              className={`pt-[26px] pb-[18px] ${
+                activeModule === "1" ? activeModuleClass : ""
+              }`}
+            >
+              <Link href={"/"}>{t("personal")}</Link>
+            </li>
+            <li
+              className={`pt-[26px] pb-[18px] ${
+                activeModule === "2" ? activeModuleClass : ""
+              }`}
+            >
+              <Link href={"/business"}>{t("business")}</Link>
+            </li>
+            <li
+              className={`pt-[26px] pb-[18px] ${
+                activeModule === "3" ? activeModuleClass : ""
+              }`}
+            >
+              <Link href={"/bank"}>{t("bank")}</Link>
+            </li>
+          </ul>
+        </div>
+        <LocaleSwicherSelect />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/localeSwicherSelect/LocaleSwicherSelect.tsx
+++ src/components/localeSwicherSelect/LocaleSwicherSelect.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Locale, useLocale } from "next-intl";
 import { usePathname, useRouter } from "@/i18n/navigation";
+import DropdownUI from "@/ui/dropdown/DropdownUI";
 // import { useParams } from "next/navigation";
 
 export default function LocaleSwicherSelect() {
@@ -20,19 +21,50 @@ export default function LocaleSwicherSelect() {
     { value: "tj", label: "Тоҷикӣ" },
   ];
 
+  const items = [
+    {
+      key: "en",
+      label: (
+        <button onClick={() => onSelectLang("en")} className="cursor-pointer">
+          English
+        </button>
+      ),
+    },
+    {
+      key: "ru",
+      label: (
+        <button onClick={() => onSelectLang("ru")} className="cursor-pointer">
+          Русский
+        </button>
+      ),
+    },
+    {
+      key: "tj",
+      label: (
+        <button onClick={() => onSelectLang("tj")} className="cursor-pointer">
+          Тоҷикӣ
+        </button>
+      ),
+    },
+  ];
+
   return (
     <div>
-      <select
-        value={locale}
-        onChange={(e) => onSelectLang(e.target.value)}
-        className="text-[#1F2937] text-[15px] leading-[19px] bg-transparent border border-gray-300 rounded p-1 cursor-pointer"
-      >
-        {options.map((el) => (
-          <option key={el.value} value={el.value}>
-            {el.label}
-          </option>
-        ))}
-      </select>
+      <DropdownUI
+        text={options.find((el) => el.value === locale)?.label || ""}
+        items={items}
+      />
+      {/* <select
+         value={locale}
+         onChange={(e) => onSelectLang(e.target.value)}
+         className="text-[#1F2937] text-[15px] leading-[19px] bg-transparent border-none rounded p-1 cursor-pointer"
+       >
+         {options.map((el) => (
+           <option key={el.value} value={el.value}>
+             {el.label}
+           </option>
+         ))}
+       </select> */}
     </div>
   );
 }
