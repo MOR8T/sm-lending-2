b1d15a90753682fc0245a166ed9e4c9b8a77e94d
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-08-15 16:17:43 +0500

    create: new page /deposits for DepositsAccount

--- src/app/[locale]/business/deposits/page.tsx
+++ src/app/[locale]/business/deposits/page.tsx
@@ -1,9 +1,12 @@
+import DepositsAccounts from '@/ui/business/depositsAccounts/DepositsAccounts'
+import { useTranslations } from 'next-intl'
 import React from 'react'
 
 export default function Deposits() {
+    const t = useTranslations()
   return (
-    <div>
-        
+    <div className=' container md:text-[40px] text-[30px] md:leading-[48px] leading-[40px] font-bold md:display2 mb-4.2 mt-1 md:my-7'>
+        <DepositsAccounts title={t("depositsAccounts.title")} />
     </div>
   )
 }
