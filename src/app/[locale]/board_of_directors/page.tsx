8565f69f0209927bd745eec4b9b8b410a597425b
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2025-08-23 13:10:53 +0500

    make Board of directors page

---
+++ src/app/[locale]/board_of_directors/page.tsx
@@ -0,0 +1,43 @@
+export default function BoardOfDirectors() {
+  return (
+    <div className="container-2md text-[#262626]">
+      <h1 className="text-[#262626] text-center md:text-[32px] text-[20px] font-semibold md:mt-[60px] mt-8 md:mb-10 mb-6 ">
+        Совет директоров
+      </h1>
+      <div className="flex flex-wrap justify-center gap-6">
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] p-[12px] md:w-[265px] w-[327px] md:h-[96px] h-[92px] md:hidden flex flex-col items-center justify-center text-center">
+          <h2 className="md:text-[20px] text-[18px] leading-6">
+            Назаров Мирзонаҷот Сафарович
+          </h2>
+        </div>
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] p-[12px] md:w-[265px] w-[327px] md:h-[96px] h-[92px] flex flex-col items-center justify-center text-center">
+          <h2 className="md:text-[20px] text-[18px] leading-6">
+            Бобоев Фарҳод Рустамович
+          </h2>
+        </div>
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] p-[12px] md:w-[265px] w-[327px] md:h-[96px] h-[92px] md:flex hidden flex-col items-center justify-center text-center">
+          <h2 className="md:text-[20px] text-[18px] leading-6">
+            Назаров Мирзонаҷот Сафарович
+          </h2>
+        </div>
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] p-[12px] md:w-[265px] w-[327px] md:h-[96px] h-[92px] flex flex-col items-center justify-center text-center">
+          <h2 className="md:text-[20px] text-[18px] leading-6">
+            Абдулхақов Одилҷон Нигматуллоевич
+          </h2>
+        </div>
+      </div>
+      <div className="flex justify-center flex-wrap mt-8 gap-y-8 gap-x-6">
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] p-[12px] md:w-[265px] w-[327px] md:h-[96px] h-[92px] flex flex-col items-center justify-center text-center">
+          <h2 className="md:text-[20px] text-[18px] leading-6">
+            Маҳмудов Мурод Қудратович
+          </h2>
+        </div>
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] p-[12px] md:w-[265px] w-[327px] md:h-[96px] h-[92px] flex flex-col items-center justify-center text-center">
+          <h2 className="md:text-[20px] text-[18px] leading-6">
+            Мадҷонов Шуҳратҷон Сафиалоевич
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/[locale]/management/page.tsx
+++ src/app/[locale]/management/page.tsx
@@ -5,7 +5,7 @@ export default function Management() {
         Руководство
       </h1>
       <div className="flex justify-center">
-        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] py-[24px] px-[12px] md:w-[294px] w-[327px] md:h-[133px] h-[123] flex flex-col items-center justify-center text-center">
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] py-[24px] px-[12px] md:w-[294px] w-[327px] md:h-[133px] h-[123px] flex flex-col items-center justify-center text-center">
           <p className="text-[14px] text-[#54545A] mb-[10px]">
             Директори генералӣ
           </p>
@@ -15,7 +15,7 @@ export default function Management() {
         </div>
       </div>
       <div className="flex md:justify-between justify-center flex-wrap mt-8 gap-8">
-        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] py-[24px] px-[12px] md:w-[294px] w-[327px] md:h-[133px] h-[123] flex flex-col items-center justify-center text-center">
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] py-[24px] px-[12px] md:w-[294px] w-[327px] md:h-[133px] h-[123px] flex flex-col items-center justify-center text-center">
           {/* <p className="text-[14px] text-[#54545A] mb-[10px]">
             Муовини директори генералӣ
           </p> */}
@@ -23,7 +23,7 @@ export default function Management() {
             Муовини директори генералӣ
           </h2>
         </div>
-        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] py-[24px] px-[12px] md:w-[294px] w-[327px] md:h-[133px] h-[123] flex flex-col items-center justify-center text-center">
+        <div className="bg-[#F5F5F5] rounded-[12px] border border-[#3980A0] py-[24px] px-[12px] md:w-[294px] w-[327px] md:h-[133px] h-[123px] flex flex-col items-center justify-center text-center">
           <p className="text-[14px] text-[#54545A] mb-[10px]">Сармуҳосиб</p>
           <h2 className="md:text-[24px] text-[20px] leading-6">
             Шарипова Гулнора Холбоевна
