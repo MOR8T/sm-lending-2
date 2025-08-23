c2f1970d4589d37bb209bd1c3716d52724e65001
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2025-08-23 13:22:13 +0500

    make structure page

---
+++ src/app/[locale]/structure/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import structure from "@/../public/images/structure.svg";
+
+export default function Structure() {
+  return (
+    <div className="container-2md text-[#262626]">
+      <h1 className="text-[#262626] md:text-center md:text-[32px] text-[20px] font-semibold md:mt-[60px] mt-8 md:mb-10 mb-6 ">
+        Структура
+      </h1>
+      <div className="md:mt-10 mt-6">
+        <Image
+          src={structure}
+          alt="structure"
+          width={846}
+          height={685}
+          className="w-full h-auto"
+        />
+      </div>
+    </div>
+  );
+}
